16c36dbdf7f2f37dbac1348b140c31cc1c575b96
lokeshrookie <rocklinglokesh@gmail.com>
2024-11-24 00:13:17 +0530

    initialized project

---
+++ Easy/Easy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Easy
+{
+    public class Easy
+    {
+        // 1. Two Sum
+        public int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> map = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int complement = target - nums[i];
+
+                if (map.ContainsKey(complement))
+                {
+                    return new int[] { map[complement], i };
+                }
+                else
+                {
+                    map.TryAdd(nums[i], i);
+                }
+            }
+            return new int[] { 0, 0 };
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using Leetcode.Easy;
+using System.ComponentModel.DataAnnotations;
+
+
+// 1. Two sum inputs:
+var nums = new int[] { 2, 7, 11, 15 };
+
+var target = 9;
+
+
+Easy easy = new Easy();
+
+var twoSumResult = easy.TwoSum(nums, target);
+
+Console.WriteLine(twoSumResult);
+
+
+// Display the elements of the result (indices of the two numbers)
+Console.WriteLine($"Indices of the two numbers: {twoSumResult[0]}, {twoSumResult[1]}");
+Console.WriteLine($"Values of the two numbers: {nums[twoSumResult[0]]}, {nums[twoSumResult[1]]}");
+
+// -------------------------------------------------------------------------------------------------------------
+
+
+
+// 2. 
+
+
+
+
+
