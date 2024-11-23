f5e7719d61fb25a4658f830b5626fd8cd44b9566
lokeshrookie <rocklinglokesh@gmail.com>
2024-11-24 01:16:25 +0530

    valid parentheses

--- Easy/Easy.cs
+++ Easy/Easy.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -28,5 +29,80 @@ namespace Leetcode.Easy
             }
             return new int[] { 0, 0 };
         }
+
+        // 2. Valid Parenthesis
+        public bool IsValid(string s)
+        {
+
+            if (string.IsNullOrEmpty(s))
+            {
+                return true;
+            }
+            if (s.Length % 2 != 0)
+            {
+                return false;
+            }
+
+            Stack<char> stack = new Stack<char>();
+
+            foreach (var c in s)
+            {
+                if (c == '(' || c == '{' || c == '[')
+                {
+                    stack.Push(c);
+                }
+                else if (stack.Count == 0)
+                {
+                    return false;
+                }
+                else if (c == ')' && stack.Peek() == '(')
+                {
+                    stack.Pop();
+                }
+                else if (c == '}' && stack.Peek() == '{')
+                {
+                    stack.Pop();
+                }
+                else if (c == ']' && stack.Peek() == '[')
+                {
+                    stack.Pop();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return stack.Count == 0;
+        }
+
+        public bool IsValidV2(string s)
+        {
+            var bracketPairs = new Dictionary<char, char> {
+            { ')', '(' },
+            { '}', '{' },
+            { ']', '[' }
+        };
+
+            Stack<char> stack = new Stack<char>();
+
+            foreach (var c in s)
+            {
+                if (bracketPairs.ContainsKey(c))
+                {
+                    if (stack.Count > 0 && stack.Peek() == bracketPairs[c])
+                        stack.Pop();
+                    else
+                        return false;
+                }
+                else
+                {
+                    stack.Push(c);
+                }
+            }
+            return stack.Count == 0;
+        }
+
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,29 +1,50 @@
 ﻿using Leetcode.Easy;
 using System.ComponentModel.DataAnnotations;
 
-
-// 1. Two sum inputs:
-var nums = new int[] { 2, 7, 11, 15 };
-
-var target = 9;
-
-
 Easy easy = new Easy();
 
-var twoSumResult = easy.TwoSum(nums, target);
 
-Console.WriteLine(twoSumResult);
+// 1. Two sum inputs:
+
+//var nums = new int[] { 2, 7, 11, 15 };
+
+//var target = 9;
 
 
-// Display the elements of the result (indices of the two numbers)
-Console.WriteLine($"Indices of the two numbers: {twoSumResult[0]}, {twoSumResult[1]}");
-Console.WriteLine($"Values of the two numbers: {nums[twoSumResult[0]]}, {nums[twoSumResult[1]]}");
+
+//var twoSumResult = easy.TwoSum(nums, target);
+
+//Console.WriteLine(twoSumResult);
+
+
+//// Display the elements of the result (indices of the two numbers)
+//Console.WriteLine($"Indices of the two numbers: {twoSumResult[0]}, {twoSumResult[1]}");
+//Console.WriteLine($"Values of the two numbers: {nums[twoSumResult[0]]}, {nums[twoSumResult[1]]}");
 
 // -------------------------------------------------------------------------------------------------------------
 
 
 
-// 2. 
+//2. Valid Parentheses
+
+/**
+Stack<int> stack = new Stack<int>();
+
+// stack.Peek(); // this will throw exception if stack is empty.
+
+
+string validParentheses = "(){}[]";
+
+string inValidParentheses = "(){}[])";
+
+
+var isValidParentheses = easy.IsValid(inValidParentheses);
+
+
+Console.WriteLine(isValidParentheses);
+
+*/
+
 
 
 
